c72a1ed3f24be0e834979314884bee5ff364e7ae
Austin <austincolepage@hotmail.com>
2022-01-02 22:56:22 -0500

    fixed API

--- src/Components/Gauge.js
+++ src/Components/Gauge.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import GaugeChart from "react-gauge-chart";
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useRef } from "react";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
-import faker from "faker";
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -17,21 +16,15 @@ import {
     Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { Button } from "@mui/material";
 
 const baseURL = "http://stock-analyzer-api-aap.herokuapp.com/data/";
 
 function Gauge() {
-    const [trendPoint, setTrendPoint] = useState();
-    const [ticker, setTicker] = useState("");
-    const [minValue, maxValue] = [-3, 3];
-
-    useEffect(() => {
-        let reqURL = baseURL + ticker;
-
-        axios.get(reqURL).then((response) => {
-            setTrendPoint(response.data);
-        });
-    }, [ticker]);
+    const [labels, setLabels] = useState();
+    const [trendPts, setTrendPts] = useState();
+    const [prices, setPrices] = useState();
+    const ticker = useRef("");
 
     ChartJS.register(
         CategoryScale,
@@ -43,24 +36,12 @@ function Gauge() {
         Legend
     );
 
-    const labels = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-    ];
-
     const data = {
         labels,
         datasets: [
             {
                 label: "Price",
-                data: labels.map(() =>
-                    faker.datatype.number({ min: -1000, max: 1000 })
-                ),
+                data: prices,
                 borderColor: "rgb(255, 99, 132)",
                 backgroundColor: "rgba(255, 99, 132, 0.5)",
             },
@@ -73,38 +54,65 @@ function Gauge() {
             legend: {
                 position: "top",
             },
+            title: {
+                display: true,
+                text: "Stock Name",
+            },
         },
+    };
+
+    const handleSubmit = (event) => {
+        setLabels([]);
+        setPrices([]);
+        let reqURL = baseURL + ticker.current.value;
+
+        axios.get(reqURL).then((response) => {
+            setLabels(response.data["datePrice"][0]);
+            setPrices(response.data["datePrice"][1]);
+            setTrendPts(response.data["trendPoints"]);
+        });
     };
 
     return (
         <div className="flex-col m-auto w-[500px] flex justify-center items-center h-screen">
             <p className="font-bold text-5xl drop-shadow-md">Stock Analyzer</p>
-            <FormControl sx={{ width: 1, marginY: 5 }}>
+            <FormControl sx={{ flexDirection: "row", width: 1, marginY: 5 }}>
                 <InputLabel htmlFor="outlined-adornment-amount">
                     Ticker
                 </InputLabel>
                 <OutlinedInput
+                    sx={{ width: 0.5 }}
+                    inputRef={ticker}
                     id="outlined-adornment-amount"
                     startAdornment={
                         <InputAdornment position="start">$</InputAdornment>
                     }
-                    value={ticker}
-                    onChange={(e) => setTicker(e.target.value)}
+                    onChange={(e) => {
+                        ticker.current.value = e.target.value;
+                    }}
                     label="Ticker"
                 />
+                <Button
+                    sx={{ width: 0.5 }}
+                    variant="outlined"
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </Button>
             </FormControl>
-            <Line className="mb-7" data={data} options={options} />
+            {trendPts && (
+                <Line className="mb-7" data={data} options={options} />
+            )}
             <GaugeChart
                 className=""
                 id="gauge-chart3"
                 nrOfLevels={4}
                 colors={["#FF5F6D", "#FFC371", "#7FF782", "#00FF06"]}
                 arcWidth={0.25}
-                percent={0.5}
+                percent={trendPts ? Math.abs(trendPts) / 3 : 0.5}
                 needleColor={"#000000"}
                 hideText
             />
-            {trendPoint ? <p>{trendPoint["data"]}</p> : null}
         </div>
     );
 }
